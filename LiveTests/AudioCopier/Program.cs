1f39ac95329bd6d8b0360a5be7568474441c01b5
Dynesshely <catrol@qq.com>
2024-07-04 02:44:56 +0800

    Use top-level statements

--- LiveTests/AudioCopier/Program.cs
+++ LiveTests/AudioCopier/Program.cs
@@ -1,40 +1,31 @@
-﻿using System;
-using NAudio.CoreAudioApi;
+﻿using NAudio.CoreAudioApi;
 using NAudio.Wave;
 
-namespace DesktopAudioCapture;
+var captureDevice = new MMDeviceEnumerator().GetDefaultAudioEndpoint(DataFlow.Capture, Role.Console);
+var playbackDevice = new MMDeviceEnumerator().GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
+var waveFormat = new WaveFormat(44100, 16, 2);
 
-public class Program
+using var capture = new WasapiCapture(captureDevice);
+
+using var player = new WasapiOut(playbackDevice, AudioClientShareMode.Shared, true, 50);
+
+capture.WaveFormat = waveFormat;
+var bufferedWaveProvider = new BufferedWaveProvider(waveFormat)
 {
-    static void Main(string[] args)
-    {
-        var captureDevice = new MMDeviceEnumerator().GetDefaultAudioEndpoint(DataFlow.Capture, Role.Console);
-        var playbackDevice = new MMDeviceEnumerator().GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
-        var waveFormat = new WaveFormat(44100, 16, 2);
+    DiscardOnBufferOverflow = true
+};
 
-        using (var capture = new WasapiCapture(captureDevice))
-        {
-            using (var player = new WasapiOut(playbackDevice, AudioClientShareMode.Shared, true, 50))
-            {
-                capture.WaveFormat = waveFormat;
-                var bufferedWaveProvider = new BufferedWaveProvider(waveFormat);
-                bufferedWaveProvider.DiscardOnBufferOverflow = true;
+capture.DataAvailable += (s, a) =>
+{
+    bufferedWaveProvider.AddSamples(a.Buffer, 0, a.BytesRecorded);
+};
 
-                capture.DataAvailable += (s, a) =>
-                {
-                    bufferedWaveProvider.AddSamples(a.Buffer, 0, a.BytesRecorded);
-                };
+player.Init(bufferedWaveProvider);
 
-                player.Init(bufferedWaveProvider);
+capture.StartRecording();
+player.Play();
 
-                capture.StartRecording();
-                player.Play();
+Console.WriteLine("按任意键停止录制...");
+Console.ReadKey();
 
-                Console.WriteLine("按任意键停止录制...");
-                Console.ReadKey();
-
-                capture.StopRecording();
-            }
-        }
-    }
-}
+capture.StopRecording();
